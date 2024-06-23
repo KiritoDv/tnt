21fc7e72b3198f2c9503bdbb85b76c28e8db338d
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2024-06-23 04:18:48 -0300

    pair more rodata files

---
+++ src/libultra/os/createmesgqueue.c
@@ -0,0 +1,3 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/libultra/os/createmesgqueue/func_800B3080.s")
---
+++ src/libultra/os/seteventmesg.c
@@ -0,0 +1,3 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/libultra/os/seteventmesg/func_800B30B0.s")
