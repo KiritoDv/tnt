620e9f8095e66401ffa148aeffc5f0d114b03984
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2024-06-23 21:53:59 -0300

    func_800572F8

--- src/1D300.c
+++ src/1D300.c
@@ -1,8 +1,6 @@
 // This file matches with -O1 -mips1
 #include "common.h"
 
-#pragma GLOBAL_ASM("asm/us/nonmatchings/1D300/func_80057080.s")
-
 void func_8004A34C(void);
 void func_800746C0(s32*, s32, s32);
 s32 func_80074888(s32*, s32);
@@ -10,6 +8,8 @@ s32 func_8007E03C(s32);
 void func_8007E328(s32);
 
 extern s32 D_273A00;
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/1D300/func_80057080.s")
 
 void func_800570B0(s32* arg0, s32 arg1) {
     func_8004A34C();
@@ -25,6 +25,9 @@ void func_80057114(s32* arg0) {
 
 #pragma GLOBAL_ASM("asm/us/nonmatchings/1D300/func_800571D4.s")
 
-#pragma GLOBAL_ASM("asm/us/nonmatchings/1D300/func_800572F8.s")
+void func_800572F8(s32* arg0) {
+    func_8007E328(arg0[0]);
+    func_8007E328(arg0[1]);
+}
 
 #pragma GLOBAL_ASM("asm/us/nonmatchings/1D300/func_80057338.s")
