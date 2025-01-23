a8e40a2e86f96b54eb534c8625577ca1d8faff58
LLONSIT <lonsitsyt456@gmail.com>
2025-01-22 20:47:22 -0500

    Code revision

--- include/functions.h
+++ include/functions.h
@@ -19,4 +19,17 @@ void Fault_Assert(char* assert, char* source);
 
 //E2A0.c
 void func_8004A5A0(char* assert_name, char* file, int line);
+
+//2F270.c
+void func_8006D674(s32);         /* extern */
+s32 func_800A35EC(void*, void*); /* extern */
+void func_8006E288(s32); /* extern */
+void func_800692FC(struct UnkStruct_80068FF0* arg0, s32 arg1);
+void func_8006D450(s32, s32);
+void func_8006D5BC(s32);
+void func_80070860(s32, void*, s32);
+
+void Fault_Asset(char*, char*, char*); /* extern */
+
+
 #endif /* FUNCTIONS_H */
